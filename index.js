801c09b2fc70d3233c34fcc29f739ef806201ee1
unknown <zhifan_shen@sfu.ca>
2020-07-06 23:56:20 -0700

    ultiplayer keyboard interactivity on canvas

--- index.js
+++ index.js
@@ -1,15 +1,80 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
+const serv = require('http').Server(app);
 
-var app = express()
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.use(express.static(path.join(__dirname, 'public')))
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
 
-app.set('views', path.join(__dirname, 'views'))
-  
-app.set('view engine', 'ejs')
-  
-app.get('/', (req, res) => res.render('pages/index'))
-  
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var Player = function(id){
+    var self = {
+        x:250,
+        y:250,
+        id:id,
+        number:"" + Math.floor(10 * Math.random()),
+        pressingRight:false,
+        pressingLeft:false,
+        pressingUp:false,
+        pressingDown:false,
+        maxSpd:10,
+    }
+    self.updatePosition = function(){
+        if(self.pressingRight)
+            self.x += self.maxSpd;
+        if(self.pressingLeft)
+            self.x -= self.maxSpd;
+        if(self.pressingUp)
+            self.y -= self.maxSpd;
+        if(self.pressingDown)
+            self.y += self.maxSpd;
+    }
+    return self;
+}
+
+const io = require('socket.io')(3000);
+io.on('connection',(socket)=>{
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;
+  var player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
+
+  socket.on('disconnect',()=>{
+    delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
+  })
+
+  socket.on('keyPress',function(data){
+        if(data.inputId === 'left')
+            player.pressingLeft = data.state;
+        else if(data.inputId === 'right')
+            player.pressingRight = data.state;
+        else if(data.inputId === 'up')
+            player.pressingUp = data.state;
+        else if(data.inputId === 'down')
+            player.pressingDown = data.state;
+    });
+});
+setInterval(()=>{
+  var pack = [];
+  for (var i in PLAYER_LIST) {
+    var player = PLAYER_LIST[i];
+    player.updatePosition();
+    pack.push({
+      x: player.x,
+      y: player.y,
+      number:player.number
+    });
+  }
+  for (var i in SOCKET_LIST){
+    var socket = SOCKET_LIST[i];
+    socket.emit('newPositions',pack);
+  }
+
+},1000/25)
