9476b52aeda180f6194f2d135d002b5273bae544
Favian Samatha <faviansamatha@yahoo.com>
2020-07-07 00:33:59 -0700

    Added undo functionality

--- public/canvas-test.js
+++ public/canvas-test.js
@@ -3,12 +3,15 @@ let isMouseDown = false;
 const context = canvas.getContext('2d');
 var eraser = document.getElementById('eraser-tool');
 var colourWheel = document.getElementById('colour-wheel');
+var undoButton = document.getElementById('undo');
 var selectedColor = colourWheel.value
 var colour = selectedColor;
 var slider = document.getElementById('brush-size');
 var clear = document.getElementById('clear')
 var brushSize = 1;
 let x,y = 0;
+let tempStack = [];
+let undoStack= [];
 
 slider.addEventListener('input',function(evt){
     brushSize = this.value;
@@ -22,12 +25,30 @@ clear.addEventListener('click',function(evt){
 eraser.addEventListener('click', function(evt){
     colour = 'white';
 })
+
+undoButton.addEventListener('click', (evt)=>{
+    undoStack.pop();
+    undoStack.pop();
+
+    context.clearRect(0,0,canvas.width,canvas.height)
+    undoStack.forEach(path=>{
+        context.beginPath();
+        context.moveTo(path[0].x,path[0].y);
+        for(let i = 1; i < path.length; i++){
+            context.lineTo(path[i].x,path[i].y);
+        }
+        context.stroke();
+    })
+})
 canvas.addEventListener('mousedown',(evt)=>{
 
     x = evt.offsetX;
     y = evt.offsetY;
     isMouseDown = true;
     context.lineJoin = context.lineCap = 'round'
+    tempStack = [];
+    tempStack.push({x,y})
+
 
 
 })
@@ -40,6 +61,7 @@ canvas.addEventListener('mousemove',(evt)=>{
         draw(context,x,y,evt.offsetX, evt.offsetY);
         x = evt.offsetX;
         y = evt.offsetY;
+        tempStack.push({x,y})
     }
 
 
@@ -53,6 +75,7 @@ window.addEventListener('mouseup',(evt)=>{
         y = 0;
         isMouseDown = false;
     }
+    undoStack.push(tempStack)
 })
 
 function draw(context,x,y,x2,y2){
@@ -64,4 +87,6 @@ function draw(context,x,y,x2,y2){
     context.stroke();
     context.closePath();
 }
+
+
 
