95c62aa74e986ac2f5d314813c720f61d69ade0f
Michael Chang <mchang0926@gmail.com>
2020-07-23 16:42:47 -0700

    commented out get definitions

--- index.js
+++ index.js
@@ -47,27 +47,27 @@ const fetch = require("node-fetch");
 /* reference on implmenting the urban dictionary api: 
    https://rapidapi.com/community/api/urban-dictionary */
 
-function get_definition(word) {
-	var req = unirest("GET", "https://mashape-community-urban-dictionary.p.rapidapi.com/define");
-	req.headers({
-		"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
-		"x-rapidapi-key": "d5e2d819d4msh7bcc051da537e83p1d5625jsn81ee7cc88779",
-		"useQueryString": true
-	});
+// function get_definition(word) {
+// 	var req = unirest("GET", "https://mashape-community-urban-dictionary.p.rapidapi.com/define");
+// 	req.headers({
+// 		"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+// 		"x-rapidapi-key": "d5e2d819d4msh7bcc051da537e83p1d5625jsn81ee7cc88779",
+// 		"useQueryString": true
+// 	});
 	
-	req.query({
-		"term": word
-	});
+// 	req.query({
+// 		"term": word
+// 	});
 	
-	req.end(function (result1) {
-		if (result1.error) throw new Error(result1.error);
-		definitions.push(result1.body.list[0].definition);
-	});
-}	
+// 	req.end(function (result1) {
+// 		if (result1.error) throw new Error(result1.error);
+// 		definitions.push(result1.body.list[0].definition);
+// 	});
+// }	
 
-for (let i = 0; i < 3; i++) {
-	get_definition(words[i]); 
-}
+// for (let i = 0; i < 3; i++) {
+// 	get_definition(words[i]); 
+// }
 
 express()
 	.use(session({
