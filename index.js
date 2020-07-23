e789bb08efa69f804025ddcae7a0239c919d2aad
Michael Chang <mchang0926@gmail.com>
2020-07-23 03:26:06 -0700

    successfully added random generate word features
    Co-authored-by: daiisyx <daisyxu981206@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,42 @@ const session = require('express-session');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 const auth = require('./authentication')
+var randWords = require('random-words')
 
+function words3(){
+	return randWords(3)
+}
+
+
+// /** urban-dictionary */
+// const Filter = require('bad-words')
+// const ud = require('urban-dictionary')
+
+// const filter = new Filter()
+
+// // Callback example.
+// ud.random((error, entry) => {
+//   if (error) {
+// 	console.error(error.message)
+// } else {
+// 	// console.log(filter.clean(entry.word))
+// 	var w = filter.clean(entry.word)
+
+// 	// console.log(filter.clean(entry.definition))
+// 	var d = filter.clean(entry.definition)
+
+//     // console.log(entry.example)
+// 	var g = {w: d}
+//   }
+// })
+
+// make list of 3 random words
+
+// insert those words through the api
+
+// output it as json
+
+// take those output to the ejs to get display on
 
 express()
 	.use(session({
@@ -24,8 +59,16 @@ express()
 	// Routes 
 	/* Home */ .get('/', auth.loadHome)
 	/* Game */ .get('/game', (req, res) => res.render('pages/game'))
-	/* Word */ .get('/choose_word', (req, res) => res.render('pages/word_list'))
-
+	/* Word */ .get('/choose_word', (req, res) => {
+													var data = words3()
+													var data0 = data[0]
+													var data1 = data[1]
+													var data2 = data[2]
+													// console.log(data0)
+													// console.log(data1)
+													// console.log(data2)
+													res.render('pages/word_list.ejs', {data0: data0, data1: data1, data2: data2})
+													})
 
 	// Start Listening 
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
