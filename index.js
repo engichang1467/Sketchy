cf8d3f3e192c318bda5d9213a84cd88c7fd10d52
Michael Chang <mchang0926@gmail.com>
2020-07-23 16:37:34 -0700

    implemented wiki api (finally)

--- index.js
+++ index.js
@@ -3,11 +3,45 @@ const session = require('express-session');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 const auth = require('./authentication')
-var randWords = require('random-words')
+// const { getRandWords} = require('./wikiDictionary')
+const fs = require('fs');
 
-function words3(){
-	return randWords(3)
+// Returns the path to the word list which is separated by `\n`
+const wordListPath = require('word-list');
+
+const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
+
+function getRandomInt(max) 
+{
+    return Math.floor(Math.random() * Math.floor(max));
 }
+
+function getRandWords()
+{
+	a = []
+	var n = getRandomInt(wordArray.length - 1)
+	// var url = `https://www.dictionary.com/browse/${wordArray[n]}`
+	// a.push(wordArray[n])
+	// a.push(url)
+    return wordArray[n]
+}
+
+const fetch = require("node-fetch");
+
+// async function get_data(query){
+//     try {
+//       const response = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${query}`);
+//       const json = await response.json();
+//       console.log(json[3][1]);
+//     } catch (error) {
+//       console.log(error);
+//     }
+// };
+  
+// var query = 'apple';
+// var url = ;
+
+// get_data(query)
 
 
 // /** urban-dictionary */
@@ -59,15 +93,49 @@ express()
 	// Routes 
 	/* Home */ .get('/', auth.loadHome)
 	/* Game */ .get('/game', (req, res) => res.render('pages/game'))
-	/* Word */ .get('/choose_word', (req, res) => {
-													var data = words3()
-													var data0 = data[0]
-													var data1 = data[1]
-													var data2 = data[2]
+	// /* Word */ .get('/choose_word', (req, res) => {
+	// 												// var data = words3()
+	// 												var data0 = getRandWords()
+	// 												var dataInfo0 = data0[1]
+	// 												var data1 = getRandWords()
+	// 												var dataInfo1 = data1[1]
+	// 												var data2 = getRandWords()
+	// 												var dataInfo2 = data2[1]
+	// 												// console.log(data0)
+	// 												// console.log(data1)
+	// 												// console.log(data2)
+	// 												res.render('pages/word_list.ejs', {data0: data0[0], data1: data1[0], data2: data2[0], dataInfo0: dataInfo0, dataInfo1: dataInfo1, dataInfo2: dataInfo2})
+	// 												})
+
+	/* Word */ .get('/choose_word', async (req, res) => {
+													// var data = words3()
+													
+
+													try {
+														var data0 = getRandWords()
+														const response = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${data0}`);
+														const json = await response.json();
+														const dataInfo0 = await json[3][1]
+
+														var data1 = getRandWords()
+														const response1 = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${data1}`);
+														const json1 = await response1.json();
+														const dataInfo1 = await json1[3][1]
+
+														var data2 = getRandWords()
+														const response2 = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${data2}`);
+														const json2 = await response2.json();
+														const dataInfo2 = await json2[3][1]
+														
+														// console.log(json[3][1]);
+														res.render('pages/word_list.ejs', {data0: data0, data1: data1, data2: data2, dataInfo0: dataInfo0, dataInfo1: dataInfo1, dataInfo2: dataInfo2})
+													} catch (error) {
+														console.log(error);
+													}
 													// console.log(data0)
 													// console.log(data1)
 													// console.log(data2)
-													res.render('pages/word_list.ejs', {data0: data0, data1: data1, data2: data2})
+													
 													})
 
 	// Start Listening 
