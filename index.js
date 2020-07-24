be64ec6689be37f9673cb92b598626052019c291
Michael Chang <mchang0926@gmail.com>
2020-07-23 19:48:09 -0700

    remove comments and unnecessary packages

--- index.js
+++ index.js
@@ -17,31 +17,6 @@ function getRandWords()
     return wordArray[n]
 }
 
-/* reference on implmenting the urban dictionary api: 
-   https://rapidapi.com/community/api/urban-dictionary */
-
-// function get_definition(word) {
-// 	var req = unirest("GET", "https://mashape-community-urban-dictionary.p.rapidapi.com/define");
-// 	req.headers({
-// 		"x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
-// 		"x-rapidapi-key": "d5e2d819d4msh7bcc051da537e83p1d5625jsn81ee7cc88779",
-// 		"useQueryString": true
-// 	});
-	
-// 	req.query({
-// 		"term": word
-// 	});
-	
-// 	req.end(function (result1) {
-// 		if (result1.error) throw new Error(result1.error);
-// 		definitions.push(result1.body.list[0].definition);
-// 	});
-// }	
-
-// for (let i = 0; i < 3; i++) {
-// 	get_definition(words[i]); 
-// }
-
 express()
 	.use(session({
 		secret: '276isthebest',
