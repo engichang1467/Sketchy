8117d9a2570fbf0fe68e00c3e7f6dcc78320d5e1
Favian Samatha <faviansamatha@yahoo.com>
2020-07-30 17:09:38 -0700

    Finished up some sockets. Drawing won't render on spectator's screen until mouse up event is activated

--- public/app.js
+++ public/app.js
@@ -130,10 +130,277 @@ function outputDisconnectMessage(message) {
 // =================
 
 
-
+// fabric.PencilBrush = fabric.util.createClass(fabric.BaseBrush, /** @lends fabric.PencilBrush.prototype */ {
+//   /**
+//    * Discard points that are less than `decimate` pixel distant from each other
+//    * @type Number
+//    * @default 0.4
+//    */
+//   decimate: 0.4,
+//   /**
+//    * Constructor
+//    * @param {fabric.Canvas} canvas
+//    * @return {fabric.PencilBrush} Instance of a pencil brush
+//    */
+//   initialize: function(canvas) {
+//     this.canvas = canvas;
+//     this._points = [];
+//   },
+//   /**
+//    * Invoked inside on mouse down and mouse move
+//    * @param {Object} pointer
+//    */
+//   _drawSegment: function (ctx, p1, p2) {
+//     var midPoint = p1.midPointFrom(p2);
+//     ctx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
+//     return midPoint;
+//   },
+//   /**
+//    * Inovoked on mouse down
+//    * @param {Object} pointer
+//    */
+//   onMouseDown: function(pointer, options) {
+//     if (!this.canvas._isMainEvent(options.e)) {
+//       return;
+//     }
+//     this._prepareForDrawing(pointer);
+//     // capture coordinates immediately
+//     // this allows to draw dots (when movement never occurs)
+//     this._captureDrawingPath(pointer);
+//     this._render();
+//     if (!this.canvas._isMainEvent(options.e)) {
+//       return true;
+//     }
+//     this.oldEnd = undefined;
+//     this._finalizeAndAddPath();
+//     return false;
+//   },
+//   /**
+//    * Inovoked on mouse move
+//    * @param {Object} pointer
+//    */
+//   onMouseMove: function(pointer, options) {
+//     if (!this.canvas._isMainEvent(options.e)) {
+//       return;
+//     }
+//     if (this._captureDrawingPath(pointer) && this._points.length > 1) {
+//       if (this.needsFullRender()) {
+//         // redraw curve
+//         // clear top canvas
+//         this.canvas.clearContext(this.canvas.contextTop);
+//         this._render();
+//       }
+//       else {
+//         var points = this._points, length = points.length, ctx = this.canvas.contextTop;
+//         // draw the curve update
+//         this._saveAndTransform(ctx);
+//         if (this.oldEnd) {
+//           ctx.beginPath();
+//           ctx.moveTo(this.oldEnd.x, this.oldEnd.y);
+//         }
+//         this.oldEnd = this._drawSegment(ctx, points[length - 2], points[length - 1], true);
+//         ctx.stroke();
+//         ctx.restore();
+//       }
+//     }
+//   },
+//   /**
+//    * Invoked on mouse up
+//    */
+//   onMouseUp: function(options) {
+//     if (!this.canvas._isMainEvent(options.e)) {
+//       return true;
+//     }
+//     this.oldEnd = undefined;
+//     this._finalizeAndAddPath();
+//     return false;
+//   },
+//   /**
+//    * @private
+//    * @param {Object} pointer Actual mouse position related to the canvas.
+//    */
+//   _prepareForDrawing: function(pointer) {
+//     var p = new fabric.Point(pointer.x, pointer.y);
+//     this._reset();
+//     this._addPoint(p);
+//     this.canvas.contextTop.moveTo(p.x, p.y);
+//   },
+//   /**
+//    * @private
+//    * @param {fabric.Point} point Point to be added to points array
+//    */
+//   _addPoint: function(point) {
+//     if (this._points.length > 1 && point.eq(this._points[this._points.length - 1])) {
+//       return false;
+//     }
+//     this._points.push(point);
+//     return true;
+//   },
+//   /**
+//    * Clear points array and set contextTop canvas style.
+//    * @private
+//    */
+//   _reset: function() {
+//     this._points = [];
+//     this._setBrushStyles();
+//     this._setShadow();
+//   },
+//   /**
+//    * @private
+//    * @param {Object} pointer Actual mouse position related to the canvas.
+//    */
+//   _captureDrawingPath: function(pointer) {
+//     var pointerPoint = new fabric.Point(pointer.x, pointer.y);
+//     return this._addPoint(pointerPoint);
+//   },
+//   /**
+//    * Draw a smooth path on the topCanvas using quadraticCurveTo
+//    * @private
+//    */
+//   _render: function() {
+//     var ctx  = this.canvas.contextTop, i, len,
+//         p1 = this._points[0],
+//         p2 = this._points[1];
+//     this._saveAndTransform(ctx);
+//     ctx.beginPath();
+//     //if we only have 2 points in the path and they are the same
+//     //it means that the user only clicked the canvas without moving the mouse
+//     //then we should be drawing a dot. A path isn't drawn between two identical dots
+//     //that's why we set them apart a bit
+//     if (this._points.length === 2 && p1.x === p2.x && p1.y === p2.y) {
+//       var width = this.width / 1000;
+//       p1 = new fabric.Point(p1.x, p1.y);
+//       p2 = new fabric.Point(p2.x, p2.y);
+//       p1.x -= width;
+//       p2.x += width;
+//     }
+//     ctx.moveTo(p1.x, p1.y);
+//     for (i = 1, len = this._points.length; i < len; i++) {
+//       // we pick the point between pi + 1 & pi + 2 as the
+//       // end point and p1 as our control point.
+//       this._drawSegment(ctx, p1, p2);
+//       p1 = this._points[i];
+//       p2 = this._points[i + 1];
+//     }
+//     // Draw last line as a straight line while
+//     // we wait for the next point to be able to calculate
+//     // the bezier control point
+//     ctx.lineTo(p1.x, p1.y);
+//     ctx.stroke();
+//     ctx.restore();
+//   },
+//   /**
+//    * Converts points to SVG path
+//    * @param {Array} points Array of points
+//    * @return {String} SVG path
+//    */
+//   convertPointsToSVGPath: function(points) {
+//     var path = [], i, width = this.width / 1000,
+//         p1 = new fabric.Point(points[0].x, points[0].y),
+//         p2 = new fabric.Point(points[1].x, points[1].y),
+//         len = points.length, multSignX = 1, multSignY = 0, manyPoints = len > 2;
+//     if (manyPoints) {
+//       multSignX = points[2].x < p2.x ? -1 : points[2].x === p2.x ? 0 : 1;
+//       multSignY = points[2].y < p2.y ? -1 : points[2].y === p2.y ? 0 : 1;
+//     }
+//     path.push('M ', p1.x - multSignX * width, ' ', p1.y - multSignY * width, ' ');
+//     for (i = 1; i < len; i++) {
+//       if (!p1.eq(p2)) {
+//         var midPoint = p1.midPointFrom(p2);
+//         // p1 is our bezier control point
+//         // midpoint is our endpoint
+//         // start point is p(i-1) value.
+//         path.push('Q ', p1.x, ' ', p1.y, ' ', midPoint.x, ' ', midPoint.y, ' ');
+//       }
+//       p1 = points[i];
+//       if ((i + 1) < points.length) {
+//         p2 = points[i + 1];
+//       }
+//     }
+//     if (manyPoints) {
+//       multSignX = p1.x > points[i - 2].x ? 1 : p1.x === points[i - 2].x ? 0 : -1;
+//       multSignY = p1.y > points[i - 2].y ? 1 : p1.y === points[i - 2].y ? 0 : -1;
+//     }
+//     path.push('L ', p1.x + multSignX * width, ' ', p1.y + multSignY * width);
+//     return path;
+//   },
+//   /**
+//    * Creates fabric.Path object to add on canvas
+//    * @param {String} pathData Path data
+//    * @return {fabric.Path} Path to add on canvas
+//    */
+//   createPath: function(pathData) {
+//     var path = new fabric.Path(pathData, {
+//       fill: null,
+//       stroke: this.color,
+//       strokeWidth: this.width,
+//       strokeLineCap: this.strokeLineCap,
+//       strokeMiterLimit: this.strokeMiterLimit,
+//       strokeLineJoin: this.strokeLineJoin,
+//       strokeDashArray: this.strokeDashArray,
+//     });
+//     if (this.shadow) {
+//       this.shadow.affectStroke = true;
+//       path.setShadow(this.shadow);
+//     }
+//     return path;
+//   },
+//   /**
+//    * Decimate poins array with the decimate value
+//    */
+//   decimatePoints: function(points, distance) {
+//     if (points.length <= 2) {
+//       return points;
+//     }
+//     var zoom = this.canvas.getZoom(), adjustedDistance = Math.pow(distance / zoom, 2),
+//         i, l = points.length - 1, lastPoint = points[0], newPoints = [lastPoint],
+//         cDistance;
+//     for (i = 1; i < l; i++) {
+//       cDistance = Math.pow(lastPoint.x - points[i].x, 2) + Math.pow(lastPoint.y - points[i].y, 2);
+//       if (cDistance >= adjustedDistance) {
+//         lastPoint = points[i];
+//         newPoints.push(lastPoint);
+//       }
+//     }
+//     if (newPoints.length === 1) {
+//       newPoints.push(new fabric.Point(newPoints[0].x, newPoints[0].y));
+//     }
+//     return newPoints;
+//   },
+//   /**
+//    * On mouseup after drawing the path on contextTop canvas
+//    * we use the points captured to create an new fabric path object
+//    * and add it to the fabric canvas.
+//    */
+//   _finalizeAndAddPath: function() {
+//     var ctx = this.canvas.contextTop;
+//     ctx.closePath();
+//     if (this.decimate) {
+//       this._points = this.decimatePoints(this._points, this.decimate);
+//     }
+//     var pathData = this.convertPointsToSVGPath(this._points).join('');
+//     if (pathData === 'M 0 0 Q 0 0 0 0 L 0 0') {
+//       // do not create 0 width/height paths, as they are
+//       // rendered inconsistently across browsers
+//       // Firefox 4, for example, renders a dot,
+//       // whereas Chrome 10 renders nothing
+//       this.canvas.requestRenderAll();
+//       return;
+//     }
+//     var path = this.createPath(pathData);
+//     this.canvas.clearContext(this.canvas.contextTop);
+//     this.canvas.add(path);
+//     this.canvas.requestRenderAll();
+//     path.setCoords();
+//     this._resetShadow();
+//     // fire event 'path' created
+//     this.canvas.fire('path:created', { path: path });
+//   }
+// });
 
 // ============
 //    CANVAS
+
 var canvas = new fabric.Canvas('draw-area');
 var canvasHtml = document.getElementById('draw-area');
 var context = canvasHtml.getContext("2d");
@@ -159,8 +426,9 @@ var eraserButton = $(document.getElementById('tool-eraser'));
 var sizeSlider = document.getElementById('tool-size-slider');
 var undoLock =true; //A lock for the undo status to solve the spamming of undo
 var redoLock =true; //A lock for the redo status to solve the spamming of redo
-var isFillOn = false;
-var canvasLock = false; 
+var isFillOn = false; 
+var canvasLock = false; //Used to make sure the other user won't keep sending a signal to the drawer
+var undoOrRedoLock = false; //Used to make sure that it doesn't re-omit when an object is added
 var color = colorButton.value;
 
 canvas.freeDrawingBrush.color = color;
@@ -195,12 +463,16 @@ var updateCanvasState = function() {
   }
 }
 
+
+//Undo logic
 var undo = function() {
+  //Uses teh undolock so that the spamming of undo won't glitch it out
   if(undoLock){
     if(indexTracker == -1){
       undoStatus = false;
     }
     else{
+      undoOrRedoLock = true
       if (canvasState.length >= 1) {
         undoLock = false;
         if(indexTracker != 0){
@@ -220,16 +492,22 @@ var undo = function() {
           indexTracker -= 1;
         }
       }
+
       
+      var data = canvas.toJSON()
+      socket.emit('mouse',data)
+      undoOrRedoLock = false
     }
     
   }
 }
 
+//Redo Logic
 var redo = function() {
   if(redoLock){
     if((indexTracker == canvasState.length-1) && indexTracker != -1){
     }else{
+      undoOrRedoLock = true
       if (canvasState.length > indexTracker && canvasState.length != 0){
         redoLock = false;
         redoStatus = true;
@@ -243,41 +521,29 @@ var redo = function() {
           redoLock = true;
         });
       }
+      var data = canvas.toJSON()
+      socket.emit('mouse',data)
+      undoOrRedoLock = false
     }
   }
 }
 
 
-//Updates the canvas state everytime a line is added
+function changeContextTop(input){
 
-var mouseDown = 0
-document.body.onmousedown = function(){
-  ++mouseDown;
+  canvas.contexTop = input
 }
-document.body.onmouseup = function(){
-  --mouseDown;
-}
-
-canvas.on(
-  'mouse:move', function(){
-      // updateCanvasState();
-      // if(mouseDown)
-      // {
-        
-        var data = canvas.toJSON()
-
-        socket.emit('mouse',data)
-      // }
-  }
-);
 
 canvas.on(
   'object:added', function(){
     if(canvasLock === false)
       {
       updateCanvasState();
-      // var data = canvas.toJSON()
-      // socket.emit('mouse',data)
+      if( undoOrRedoLock === false)
+      { 
+          var data = canvas.toJSON()
+          socket.emit('mouse',data)
+      }
       }
   }
 );
@@ -292,8 +558,12 @@ redoButton.click(function(){
 
 clearEl.click(function() { 
  
+  undoOrRedoLock = true
   updateCanvasState();
   canvas.clear() 
+  var data = canvas.toJSON()
+  socket.emit('mouse',data)
+  undoOrRedoLock = false
 
 }
  
@@ -307,177 +577,12 @@ clearEl.click(function() {
 
 
 socket.on('mouse',(data)=>{
-  console.log("HELLO")
-  // canvasLock = true
-  // var canvasAsJson = JSON.stringify(data);
-  // canvas.loadFromJSON(canvasAsJson)
-  // canvas.renderAll();
+
+  canvasLock = true
+  var canvasAsJson = JSON.stringify(data);
+  canvas.loadFromJSON(canvasAsJson)
+  canvas.renderAll();
 })
-
-// socket.on('mouse',(data) =>{
-//   receive_draw(data.x,data.y,data.px,data.py,data.color,data.size);
-// })
-
-// socket.on('clear',() =>{
-//   context.clearRect(0,0,canvas.width,canvas.height);
-// })
-
-// socket.on('undo',(stack) =>{
-//   context.clearRect(0,0,canvas.width,canvas.height);
-//   stack.forEach(path=>{
-//       context.beginPath();
-//       context.strokeStyle = path[0].colour;
-//       context.lineWidth = path[0].brushSize;
-//       context.moveTo(path[0].x,path[0].y);
-//       for(let i = 1; i < path.length; i++){
-//           context.lineTo(path[i].x,path[i].y);
-//       }
-//       context.stroke();
-//   })
-// })
-
-
-
-
-
-// const canvas = document.getElementById('draw-area');
-// let isMouseDown = false;
-// const context = canvas.getContext('2d');
-// var eraser = document.getElementById('tool-eraser');
-// //var colourWheel = document.getElementById('tool-color');
-// var undoButton = document.getElementById('tool-undo');
-// var selectedColor = '#000'
-// var colour = selectedColor;
-// //var slider = document.getElementById('tool-size');
-// var clear = document.getElementById('tool-clear')
-// var brushSize = 1;
-// let x,y = 0;
-// let tempStack = [];
-// let undoStack= [];
-
-// // slider.addEventListener('input',function(evt){
-// //     brushSize = this.value;
-// // })
-// // colourWheel.addEventListener('input', function(evt){
-// //     colour = this.value;
-// // })
-// clear.addEventListener('click',function(evt){
-//     context.clearRect(0,0,canvas.width,canvas.height);
-//     undoStack = [];
-//     sendclear();
-// })
-// eraser.addEventListener('click', function(evt){
-//     colour = 'white';
-// })
-
-// undoButton.addEventListener('click', (evt)=>{
-//     undoStack.pop();
-//     undoStack.pop();
-//     context.clearRect(0,0,canvas.width,canvas.height);
-//     undoStack.forEach(path=>{
-//         // context.lineJoin = context.lineCap = 'round';
-//         context.beginPath();
-//         context.strokeStyle = path[0].colour;
-//         context.lineWidth = path[0].brushSize;
-//         context.moveTo(path[0].x,path[0].y);
-//         for(let i = 1; i < path.length; i++){
-//             context.lineTo(path[i].x,path[i].y);
-//         }
-//         context.stroke();
-//     })
-//     socket.emit('undo',undoStack);
-// })
-// canvas.addEventListener('mousedown',(evt)=>{
-
-//     x = evt.offsetX;
-//     y = evt.offsetY;
-//     isMouseDown = true;
-//     context.lineJoin = context.lineCap = 'round'
-//     tempStack = [];
-
-//     tempStack.push({x,y,colour,brushSize})
-
-// })
-
-
-// canvas.addEventListener('mousemove',(evt)=>{
-//     if(isMouseDown){
-//         console.log("MOUSE DOWN")
-//         draw(context,x,y,evt.offsetX, evt.offsetY);
-//         x = evt.offsetX;
-//         y = evt.offsetY;
-//         tempStack.push({x,y,colour,brushSize})
-
-//     }
-// });
-
-// window.addEventListener('mouseup',(evt)=>{
-//     if (isMouseDown){
-//         draw(context,x,y,evt.offsetX, evt.offsetY);
-//         x = 0;
-//         y = 0;
-//         isMouseDown = false;
-//     }
-//     undoStack.push(tempStack)
-// })
-
-// function draw(context,x,y,x2,y2){
-//     context.beginPath();
-//     context.strokeStyle = colour;
-//     context.lineWidth = brushSize;
-//     context.moveTo(x, y);
-//     context.lineTo(x2, y2);
-//     context.stroke();
-//     context.closePath();
-//     sendmouse(x,y,x2,y2,colour,brushSize);
-// }
-// function sendmouse(x,y,px,py,color,size){
-//   const data = {
-//     x:x,
-//     y:y,
-//     px:px,
-//     py:py,
-//     color: color,
-//     size:size
-//   }
-//   socket.emit('mouse',data);
-// }
-// function sendclear(){
-//   socket.emit('clear');
-// }
-// function receive_draw(x,y,px,py,color,size){
-//   colour = color;
-//   brushSize = size;
-//   context.beginPath();
-//   context.strokeStyle = colour;
-//   context.lineWidth = brushSize;
-//   context.moveTo(x, y);
-//   context.lineTo(px,py);
-//   context.stroke();
-//   context.closePath();
-// }
-
-// socket.on('mouse',(data) =>{
-//   receive_draw(data.x,data.y,data.px,data.py,data.color,data.size);
-// })
-
-// socket.on('clear',() =>{
-//   context.clearRect(0,0,canvas.width,canvas.height);
-// })
-
-// socket.on('undo',(stack) =>{
-//   context.clearRect(0,0,canvas.width,canvas.height);
-//   stack.forEach(path=>{
-//       context.beginPath();
-//       context.strokeStyle = path[0].colour;
-//       context.lineWidth = path[0].brushSize;
-//       context.moveTo(path[0].x,path[0].y);
-//       for(let i = 1; i < path.length; i++){
-//           context.lineTo(path[i].x,path[i].y);
-//       }
-//       context.stroke();
-//   })
-// })
 
 //    END OF CANVAS
 // ===================
