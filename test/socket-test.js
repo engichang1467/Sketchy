155cc6ce2e7d932299184285c98f2e74329e861e
Favian Samatha <faviansamatha@yahoo.com>
2020-08-03 17:17:20 -0700

    Added third socket to demonstrate that rooms are independant

--- test/socket-test.js
+++ test/socket-test.js
@@ -4,11 +4,12 @@ var expect = require('chai').expect;
 
 
 
-describe('Testing Sockets and chat room', function() {
+describe('Testing Game Room Sockets', function() {
 
     var socket1;
     var socket2;
     var socket3;
+    var s3Msgs= 0;
     it('User succesfully joins a room', function(done) {
       
       socket1 = io.connect('http://localhost:5000', {
@@ -31,7 +32,7 @@ describe('Testing Sockets and chat room', function() {
         
     });
 
-    it('User succesfully receives a message from another user',function(done){
+    it('User succesfully receives a message from another user in the same room',function(done){
       var counter = 0;
       socket1 = io.connect('http://localhost:5000', {
                 'reconnection delay' : 0
@@ -51,37 +52,63 @@ describe('Testing Sockets and chat room', function() {
           }
           else if (counter ==2){
             expect(msg.content).to.equal('Hello There')
-            console.log(msg)
           }
           counter++;
           if (counter == 3)
           {
-            socket1.disconnect();
-            console.log("disconnecting socket1...")
             socket2.disconnect();
             console.log("disconnecting socket2...")
             
           }
+          if (counter ==4)
+          {
+              expect(msg.content).to.equal('tester2 has left the game!')
+              socket1.disconnect();
+              console.log("disconnecting socket1...")
+          }
           
         })
         var session = {username: "tester1", currentRoom: 1}
-        
         socket1.emit('addUserToRoom',{session});
+
         socket2 = io.connect('http://localhost:5000', {
                 'reconnection delay' : 0
                 , 'reopen delay' : 0
                 , 'force new connection' : true
             });
         socket2.on('connect',function(done){
-          var session = {username: "tester2", currentRoom: 1}
-          var message = "Hello There"
-          socket2.emit('addUserToRoom',{session});
-          var msg = "Hello There"
-          socket2.emit('chatMessage',msg);
+          
+         
+          socket3 = io.connect('http://localhost:5000', {
+            'reconnection delay' : 0
+            , 'reopen delay' : 0
+            , 'force new connection' : true
+        });
+          socket3.on('connect', function(done){
+            var session = {username: "tester3", currentRoom: 2}
+            socket3.emit('addUserToRoom', {session})
+            socket3.on('message', function(msg){
+              s3Msgs++;
+              console.log(msg)
+            })
+            var msg = "Hi is this the right room?";
+            socket3.emit('chatMessage',msg);
+
+            var session = {username: "tester2", currentRoom: 1}
+            socket2.emit('addUserToRoom',{session});
+            var msg = "Hello There"
+            socket2.emit('chatMessage',msg);
+            
+
+
+          })
+
+
+
         })
         
       })
       done();
-    })
-
+    });
 });
+
