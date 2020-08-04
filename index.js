44b82a5ebf7b24ae13800c631fbffa08bc03c6ed
Michael Chang <mchang0926@gmail.com>
2020-08-03 18:34:51 -0700

    adding definitions and word-pictionary-list into the function getRandomWords

--- index.js
+++ index.js
@@ -27,20 +27,20 @@ games['2'] = new Game(2, 3)
 games['3'] = new Game(3, 3)
 
 
-const wordListPath = require('word-list');
-const fs = require('fs');
 const fetch = require("node-fetch");
-const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
 
-async function getRandomWords(word_count) {
+async function getRandomWords(wordCount) 
+{
 	words = []
-	for (let i = 0; i < word_count; i++) {
-		var n = Math.floor(Math.random() * Math.floor(wordArray.length - 1));
-		random_word = wordArray[n]
-		let word_data = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${random_word}`);
-		let word_data_json = await word_data.json();
+	for (let i = 0; i < wordCount; i++) 
+	{
+		randomWord = randomPictionaryList()
+		let wordLink = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${randomWord}`);
+		let wordLinkJson = await wordLink.json();
+		let wordDef = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`)
+		let wordDefJson = await wordDef.json();
 
-		let word = {word: random_word, link: word_data_json[3][0]}
+		let word = {word: randomWord, definition: wordDefJson[0]['meanings'][0]['definitions'][0]["definition"], link: wordLinkJson[3][0]}
 		
 		words.push(word)
 	}
